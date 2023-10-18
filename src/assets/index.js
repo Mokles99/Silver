fe23673f773194c833c8a3dfd6671e1db2c6c848
Mokhles <mokles.benali900@outlook.com>
2023-10-18 19:36:47 +0100

    fst commit

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import logosls from "./logosls.svg"
+import logoslss from './hero23.png'
+import robot2 from './hero22.png'
+import home from './home2.png'
+import sls1 from  './SLS1.png'
+import sls from './sls.png'
+import LOGO from './logo111.png'
+import LOGO1 from './logo222.png'
+import LOGO2 from './logo333.png'
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  LOGO,
+  LOGO1,
+  LOGO2,
+  sls,
+  sls1,
+  logo,
+  logosls,
+  logoslss,
+  robot2,
+  quotes,
+  robot,
+  home,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,33 @@
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
+
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
+  >
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>
+        The Driving Force
+        <br className="sm:block hidden" /> Behind Excellence
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Harness the knowledge of our dedicated experts to enhance your
+          business strategies and thrive worldwide
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap  justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Testimonials;
